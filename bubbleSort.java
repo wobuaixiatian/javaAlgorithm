e02bd0f42e48f8762113846bafad0084f10258eb
hub <472238458@qq.com>
2017-11-15 15:08:12 +0800

    冒泡排序
    依次比较相邻的两个元素，如果第一个元素大于第二个元素就交换它们的位置。这样比较一轮之后，最大的元素就会跑到队尾。然后对未排序的序列重复这个过程，最终转换成有序序列。

---
+++ bubbleSort.java
@@ -0,0 +1,12 @@
+public static int[] bubbleSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++) {  
+            for (int j = 0; j < arr.length - i -1; j++) {  
+                if (arr[j] > arr[j + 1]){  
+                    arr[j] = arr[j] + arr[j + 1];  
+                    arr[j + 1] = arr[j] - arr[j + 1];  
+                    arr[j] = arr[j] - arr[j + 1];  
+                }  
+            }  
+        }  
+        return arr;  
+    }  
